b09fc030b7dae00f89db7369b2a4b19023f905df
acgrafic <andre.coelho@nkey.com.br>
2019-06-08 10:13:02 -0300

    Foodtruck manager (first version) done.

---
+++ _SAbootstrap/js/classes.js
@@ -0,0 +1,12 @@
+class Foodtruck {
+    constructor(codigo_foodtruck,nome_foodtruck,responsavel,data,telefone,login,senha) {
+      this.codigo_foodtruck = codigo_foodtruck;
+      this.nome_foodtruck = nome_foodtruck;
+      this.responsavel = responsavel;
+      this.data = data;
+      this.telefone = telefone;
+      this.login = login;
+      this.senha = senha;
+      this.produtos = [];
+    }
+  }
--- _SAbootstrap/js/foodtruck-manager.js
+++ _SAbootstrap/js/foodtruck-manager.js
@@ -1,134 +1,147 @@
-/*function cadastroFoodTruck(){
-            if (typeof(Storage)!=="undefined") {
-                if(localStorage.cont){
-                    localStorage.cont=Number(localStorage.cont)+1;
-                }else{
-                    localStorage.cont = 1;
-                }
-            var cad = document.getElementById('nomeFoodTruck').value+";"+document.getElementById('codigo').value+";"+document.getElementsByClassName('tipoProduto').value+";"+document.getElementById('nomeResponsavel').value+";"+document.getElementById('telefoneResponsavel').value+";"+document.getElementById('emailLogin').value+";"+document.getElementById('dataEntrada').value;
-
-            var cad_json = JSON.stringify(cad);
-            localStorage.setItem("cad"+localStorage.cont, cad_json);
-
-            var form = document.getElementById("fconf");
-            form.style.display="none";
-
-            var span = document.getElementById("mensagem");
-            span.innerHTML = "Dados salvos!";
-
-            }else{
-            }
-}
-
-
-function mostraCadastro(){
-        var form=document.getElementById("fconf");
-        form.style.display="block"; 
-}
-
-
-function lerCadastro(){
-            var cad_json = localStorage.getItem("cad");
-            var cad = JSON.parse(cad_json);
-            var span = document.getElementById("mensagem");
-            span.innerHTML = "Não consigo fazer esse trem funcionar";
-}
-
-
-function apagarCadastro(){
-            localStorage.removeItem("cad");
-            var span = document.getElementById("mensagem");
-            span.innerHTML = "Também não consigo :(";       
-}*/
-
 var listfoodtruckLocalStorage = [];
 listFoodtruck();
 
 
-function listFoodtruck()
-{   
+function listFoodtruck() {
     listfoodtruckLocalStorage = JSON.parse(localStorage.getItem('foodtruck'));
     //salvando o foodtruck no localstorage
-    if (typeof(Storage)!=="undefined") {
+    if (typeof (Storage) !== "undefined") {
 
-        if(typeof(listfoodtruckLocalStorage) == null)
-        {
+        if (listfoodtruckLocalStorage == null) {
             listfoodtruckLocalStorage = [];
         }
 
     }
 
-    for (var i = 0; i < listfoodtruckLocalStorage.length ; i++) {
-    {
-        var html = '<div class="col-sm-3">' +
-        '<div class="card text-center" style="width:15rem; height:20rem;">'+
-        '<div class="card text-center" style="width:15rem; height:20rem;">'+
-        '<div class="card-header text-white bg-dark mb-2">'+
-        '<h4 class="text">'+listfoodtruckLocalStorage[i].nome_foodtruck+'/h4>'+
-        '</div>'+
-        '<div class="card-body">'+
-           '<h6 class="card-subtitle mb-3 text-muted">'+listfoodtruckLocalStorage[i].codigo_foodtruck+'</h6>'+
-            '<ul class="list-group list-group-flush">'+
-            '<li class="list-group-item">'+listfoodtruckLocalStorage[i].data+'</li>'+
-            '<li class="list-group-item">'+listfoodtruckLocalStorage[i].responsavel+'</li>'+
-            '<li class="list-group-item">'+listfoodtruckLocalStorage[i].telefone+'</li>'+
-            '<li class="list-group-item">'+listfoodtruckLocalStorage[i].login+'</li>'+
-            '</ul>'+
-            '</div>'+
-            '<div>'+
-            '<button type="button" class="btn btn-secondary btn-small" data-toggle="modal" data-target="#editModal" onclick="javascript:getEditFoodtruck('+i+')">Editar</button>'+
-            '<button type="button" class="btn btn-danger btn-small" data-toggle="modal" data-target="#delModal">Excluir</button><br>'+
-            '</div>'+
-            '</div>'+
+    //atribuindo a quantidade de foodtrucks p/ o label da template
+    $("#quantidadeFoodtrucks").html(listfoodtruckLocalStorage.length.toString());
+
+    var html = "";
+
+    for (var i = 0; i < listfoodtruckLocalStorage.length; i++) {
+        html += '<div class="col-sm-3">' +
+            '<div class="card text-center" style="width:15rem; height:24rem;">' +
+            '<div class="card-header text-white bg-dark mb-2">' +
+            '<h4 class="text">' + listfoodtruckLocalStorage[i].nome_foodtruck + '</h4>' +
+            '</div>' +
+            '<div class="card-body">' +
+            '<h6 class="card-subtitle mb-3 text-muted">' + listfoodtruckLocalStorage[i].codigo_foodtruck + '</h6>' +
+            '<ul class="list-group list-group-flush">' +
+            '<li class="list-group-item">' + listfoodtruckLocalStorage[i].data + '</li>' +
+            '<li class="list-group-item">' + listfoodtruckLocalStorage[i].responsavel + '</li>' +
+            '<li class="list-group-item">' + listfoodtruckLocalStorage[i].telefone + '</li>' +
+            '<li class="list-group-item">' + listfoodtruckLocalStorage[i].login + '</li>' +
+            '</ul>' +
+            '<div class="btnCard">' +
+            '<button type="button" class="btn btn-secondary btn-small" onclick="javascript:getEditFoodtruck(' + i + ')">Editar</button>' +
+            '<button type="button" class="btn btn-danger btn-small" onclick="javascript:getDelFoodtruck(' + i + ')">Excluir</button><br>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
             '</div>';
     }
 
+    $("#listFoodtruck").append(html);
+
 }
 
-function createFoodtruck()
-{
-    //recuperar os dados do formulario
-    var foodtruck = new Foodtruck($("#codigo_foodtruck"),$("#nome_foodtruck"),$("#responsavel"),$("#data"),$("#telefone"),$("#login"),$("#senha"));
-
+function createFoodtruck() {
     //criar regra para validar unicidade de codigo
 
 
     //salvando o foodtruck no localstorage
-    if (typeof(Storage)!=="undefined") {
 
-        listfoodtruckLocalStorage = JSON.parse(localStorage.getItem('foodtruck'));
+    listfoodtruckLocalStorage = JSON.parse(localStorage.getItem('foodtruck'));
 
-        if(typeof(listfoodtruckLocalStorage) == null)
-        {
-            listfoodtruckLocalStorage = [];
-        }
-
-        listfoodtruckLocalStorage.push(foodtruck);
-
-        var listFoodtruckSetLocalStorage =  JSON.stringify(listfoodtruckLocalStorage);
-
-        localStorage.setItem('foodtruck',listFoodtruckSetLocalStorage);
-   
+    if (listfoodtruckLocalStorage == null) {
+        listfoodtruckLocalStorage = [];
     }
 
-    listFoodtruck();
-    //var foodtruckJson = JSON.stringify(foodtruck);
+    //atribuindo o id de forma automatica - vai sempre ser o max + 1
+    var nextId = listfoodtruckLocalStorage.length + 1;
+
+    //recuperar os dados do formulario
+    var foodtruck = new Foodtruck(nextId, $("#nome_foodtruck").val(), $("#responsavel").val(), $("#data").val(), $("#telefone").val(), $("#login").val(), $("#senha").val());
+
+    listfoodtruckLocalStorage.push(foodtruck);
+
+    var listFoodtruckSetLocalStorage = JSON.stringify(listfoodtruckLocalStorage);
+
+    localStorage.setItem('foodtruck', listFoodtruckSetLocalStorage);
+
+    alert("Cadastro realizado com sucesso");
+
+    //recarrega a pagina
+    location.reload();
 
 }
 
-function getEditFoodtruck(id)
-{
+function getEditFoodtruck(id) {
+
     var foodtruckEdit = listfoodtruckLocalStorage[id];
+
+    $("#codigo_foodtruck").val(foodtruckEdit.codigo_foodtruck);
+    $("#position_foodtruck_array").val(id);
+    $("#nome_foodtruck").val(foodtruckEdit.nome_foodtruck);
+    $("#data").val(foodtruckEdit.data);
+    $("#responsavel").val(foodtruckEdit.responsavel);
+    $("#telefone").val(foodtruckEdit.telefone);
+    $("#login").val(foodtruckEdit.login);
+    $("#senha").val(foodtruckEdit.senha);
+
+    $('#cadModal').modal('show');
 }
 
 
-function editFoodtruck()
-{
+function createOrEditFoodtruck() {
+    if ($("#codigo_foodtruck").val() !== null) { saveEditFoodtruck(); }
+    else { createFoodtruck(); }
+}
+
+
+function saveEditFoodtruck() {
+
+    var foodTruckEdited = new Foodtruck ($("#codigo_foodtruck").val(),$("#nome_foodtruck").val(), $("#responsavel").val(), $("#data").val(), $("#telefone").val(), $("#login").val(), $("#senha").val());
+
+    listfoodtruckLocalStorage[$("#position_foodtruck_array").val()] = foodTruckEdited;
+
+    var listFoodtruckSetLocalStorage = JSON.stringify(listfoodtruckLocalStorage);
+
+    localStorage.setItem('foodtruck', listFoodtruckSetLocalStorage);
+
+    alert("Edição realizada com sucesso");
+
+    // resetando os valores
+    $("#codigo_foodtruck").val(null);
+    $("#position_foodtruck_array").val(null);
+
+    //recarrega a pagina
+    location.reload();
 
 }
 
-function deleteFoodtruck()
-{
 
+function getDelFoodtruck(id) {
+    $("#position_foodtruck_array_delete").val(id);
+    $('#delModal').modal('show');
 }
 
+
+function deleteFoodtruck() {
+
+    //remove o item do array
+    listfoodtruckLocalStorage.splice($("#position_foodtruck_array_delete").val(), 1);
+
+    var listFoodtruckSetLocalStorage = JSON.stringify(listfoodtruckLocalStorage);
+
+    localStorage.setItem('foodtruck', listFoodtruckSetLocalStorage);
+
+    alert("Exclusão realizada com sucesso");
+
+    $("#position_foodtruck_array_delete").val(null);
+
+    //recarrega a pagina
+    location.reload();
+
+
+}

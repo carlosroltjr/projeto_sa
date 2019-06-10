819a7c5c8a5299c728bd53c356cc0ec80fdbfe27
Thiago Panchiniak <panchiniak@gmail.com>
2019-06-10 14:12:52 -0300

    modal e js no cadastro produtos

---
+++ _SAbootstrap/js/product-manager.js
@@ -0,0 +1,34 @@
+class Product {
+    constructor (idProduct, nameProduct, priceProduct, stock){
+        this.idProduct = idProduct;
+        this.nameProduct = nameProduct;
+        this.priceProduct = priceProduct;
+        this.stock = stock;
+        this.vendas = [];
+    }
+
+}  
+
+var listProductLocalStorage = [];
+listProduct();
+
+function listProduct(){
+
+}
+
+
+function inserProduct(){
+
+}
+
+function getEditProduct(idProduct){
+
+}
+
+function createEditProduct(){
+
+}
+
+function saveEditProduct(){
+    
+}

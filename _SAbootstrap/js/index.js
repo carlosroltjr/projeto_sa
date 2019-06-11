3b3b908536d3beda496706f361a526a8b3d71c7b
carlosroltjr <32556410+carlosroltjr@users.noreply.github.com>
2019-06-11 01:38:10 -0300

    Update index.js

--- _SAbootstrap/js/index.js
+++ _SAbootstrap/js/index.js
@@ -6,23 +6,3 @@ aparecer e desaparecer conforme o usuário logado é ou não admim.
 temos que tratar Food Trucks e Produtos, admin n pode ver Produtos
 e usuário n pode Food Trucks.
 */
-
-for (i=0; i<localStorage.length; i++){
-  if (localStorage.key[i] === "logged"){
-
-    var menu_li = document.createElement("li");
-    menu_li.className = "nav-item";
-    document.getElementById("menu").appendChild(menu_li);
-
-    var menu_a = document.createElement("a");
-    menu_a.className = "nav-link";
-    document.getElementById("menu_li").appendChild(menu_a);
-
-    var menu_i = document.createElement("i");
-    menu_i.className = "fas fa-fw fa-tachometer-alt";
-    document.getElementById("menu_a").appendChild(menu_i);
-
-    var menu_span = document.createElement("span");
-
-  }
-}

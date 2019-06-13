3c285e3baeb29f06c9a3ed05db56c034484c451f
Pamylla <49075654+Pamylla@users.noreply.github.com>
2019-06-13 15:59:59 -0300

    grafico exemplo
    só um exemplo de como podemos fazer um grafico

---
+++ _SAbootstrap/js/charts.js
@@ -0,0 +1,25 @@
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawVisualization);
+
+      function drawVisualization() {
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'FoodTruck 1', 'FoodTruck 2 ', 'FoodTruck 3', 'FoodTruck 4', 'FoodTruck 5', 'FoodTruck 6'],
+          ['2004/05',  165,      938,         522,             998,           450,      614.6],
+          ['2005/06',  135,      1120,        599,             1268,          288,      682],
+          ['2006/07',  157,      1167,        587,             807,           397,      623],
+          ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+          ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+        ]);
+
+        var options = {
+          title : 'Venda por mês dos Food Trucks',
+          vAxis: {title: 'Venda'},
+          hAxis: {title: 'Mês'},
+          seriesType: 'bars',
+          series: {5: {type: 'line'}}
+        };
+
+        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }

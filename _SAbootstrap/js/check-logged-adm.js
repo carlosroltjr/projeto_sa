09ca05fd0c43be60f235367db37be33d48fe47bf
Carlos De Rolt Jr <carlosrderolt@gmail.com>
2019-06-17 20:06:15 -0300

    update check-logged-adm.js

--- _SAbootstrap/js/check-logged-adm.js
+++ _SAbootstrap/js/check-logged-adm.js
@@ -1,16 +1,14 @@
 
-checkLogged = JSON.parse(localStorage.getItem("logged"));
+isLogged = JSON.parse(localStorage.getItem("logged"));
 checkAdmin = JSON.parse(localStorage.getItem("checkAdmin"));
 
-if (!checkLogged){
-  alert("Você não está logado!");
-  window.open('login.html', '_self');
-}
-
-if (checkAdmin){
-  if (checkAdmin === true){
+if (isLogged){
+  if (checkAdmin){
     //desabilita botão de cadastrar produtos
   } else{
     //desabilita botão de cadastrar foodtrucks
   }
+} else{
+  alert("Você não está logado!");
+  window.open('login.html', '_self');
 }

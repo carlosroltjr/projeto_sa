64cd99f534dc6b8903ec7af36d097500d98c0d4e
Carlos De Rolt Jr <carlosrderolt@gmail.com>
2019-06-18 20:05:49 -0300

    commit

--- _SAbootstrap/js/check-logged-adm.js
+++ _SAbootstrap/js/check-logged-adm.js
@@ -7,10 +7,7 @@ if (isLogged){
     document.getElementById('products').onclick = function(){ return false }
   } else{
     document.getElementById('foodtrucks').onclick = function(){ return false }
-<<<<<<< HEAD
     document.getElementById('sales').onclick = function(){ return false }
-=======
->>>>>>> fa8ed899ff911782e60adddc415d93b6ed77a58f
   }
 } else{
   alert("Você não está logado!");

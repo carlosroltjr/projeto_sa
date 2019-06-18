2f23f72b3110e07fd87e0ed68a19a9d8e76bd386
Carlos De Rolt Jr <carlosrderolt@gmail.com>
2019-06-18 20:03:56 -0300

    adicionando id no botão vendas para ser tratado conforme adm ou não

--- _SAbootstrap/js/check-logged-adm.js
+++ _SAbootstrap/js/check-logged-adm.js
@@ -7,6 +7,7 @@ if (isLogged){
     document.getElementById('products').onclick = function(){ return false }
   } else{
     document.getElementById('foodtrucks').onclick = function(){ return false }
+    document.getElementById('sales').onclick = function(){ return false }
   }
 } else{
   alert("Você não está logado!");

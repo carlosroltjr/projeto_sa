af16f22df9c2e8a139c7bfa40e9407da4ccabea7
Thiago Panchiniak <panchiniak@gmail.com>
2019-06-19 15:45:38 -0300

    atualização sale

--- _SAbootstrap/js/sales.js
+++ _SAbootstrap/js/sales.js
@@ -14,7 +14,7 @@ function salesProductsList(){
     var html = "";
 
     for (var i=0; i<listProductLocalStorage.length; i++) {
-        html += '<tr>' + '<th>'+listProductLocalStorage[i].id_Product+'</th>' + '<th>'+listProductLocalStorage[i].productName+'</th>'+ '<th>'+listProductLocalStorage[i].productType+'</th>'+ '<th>'+listProductLocalStorage[i].productPrice+'</th>'+ '<th>'+listProductLocalStorage[i].productStock+'</th>'+ '<th>'+'<input type="number" id="qtdeOrder'+i+'" class="input-number" max="'+ listProductLocalStorage[i].productStock +'"></input>' +'</th>' + '<th>'+ '<button type="button" class="btn btn-danger btn-small" onclick="javascript:getSales(' + listProductLocalStorage[i].id_Product + ')">Adicionar Produto</button><br>' +'</th>'+ '</tr>';
+        html += '<tr>' + '<th>'+listProductLocalStorage[i].id_Product+'</th>' + '<th>'+listProductLocalStorage[i].productName+'</th>'+ '<th>'+listProductLocalStorage[i].productType+'</th>'+ '<th>'+listProductLocalStorage[i].productPrice+'</th>'+ '<th>'+listProductLocalStorage[i].productStock+'</th>'+ '<th>'+'<input type="number" id="qtdeOrder'+i+'" class="input-number" max="'+ listProductLocalStorage[i].productStock +'"></input>' +'</th>' + '<th>'+ '<button type="button" class="btn btn-info btn-small" onclick="javascript:getSales(' + listProductLocalStorage[i].id_Product + ')">Adicionar Produto</button><br>' +'</th>'+ '</tr>';
     }
 
     $("#listSales").append(html);
@@ -23,43 +23,17 @@ function salesProductsList(){
 function getSales(id_Product) {
     listProductLocalStorage = JSON.parse(localStorage.getItem('product'));
 
-    var htmlSales = "";
+    var saleValue = "";
     var pedido = document.getElementById("qtdeOrder"+(id_Product-1)).value;
     var somaPedido = 0; 
     somaPedido += (listProductLocalStorage[id_Product-1].productPrice*pedido); 
 
-    if (htmlSales ===""){
-        htmlSales += '<div class = "col-sm-3">'+
-        '<div class = "card text-center" style = "width:15rem;height:15rem;">'+ 
-        '<div class="card-header text-white bg-dark mb-2">'+
-        '<h4 class="text">'+
-        'Informações da Venda'+
-        '</h4>' +
-        '</div>' + 
-        '<div class="card-body">'+'<h6 class="card-subtitle mb-3 text-muted">'+
-        'R$'+somaPedido + '</h6>'+ '<div class="btnCard">' +
-        '<button type="button" class="btn btn-secondary btn-small" onclick="javascript:saveSales(' + id_Product + ')">Confirmar Venda</button>'+'</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
+
+    saleValue = somaPedido;
     
 
-    $("#confirmSale").append(htmlSales);
-    } else {
-        htmlSales += '<div class = "col-sm-3">'+
-        '<div class = "card text-center" style = "width:15rem;height:15rem;">'+ 
-        '<div class="card-header text-white bg-dark mb-2">'+
-        '<h4 class="text">'+
-        'Informações da Venda'+
-        '</h4>' +
-        '</div>' + 
-        '<div class="card-body">'+'<h6 class="card-subtitle mb-3 text-muted">'+
-        'R$'+somaPedido + '</h6>'+ '<div class="btnCard">' +
-        '<button type="button" class="btn btn-secondary btn-small" onclick="javascript:saveSales(' + id_Product + ')">Confirmar Venda</button>'+'</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
-    }
+    $("#saleValue").val(saleValue);
+    
     
    
 }

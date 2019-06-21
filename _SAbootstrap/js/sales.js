aac0819949fa39bf2387f43d5a248a4004a42cfc
Fanny Cacilie <49699253+docfunny@users.noreply.github.com>
2019-06-21 20:16:44 -0300

    Updating sales js

--- _SAbootstrap/js/sales.js
+++ _SAbootstrap/js/sales.js
@@ -15,25 +15,31 @@ function salesProductsList(){
     var html = "";
 
     for (var i=0; i<listProductLocalStorage.length; i++) {
-        html += '<tr>' + '<th>'+listProductLocalStorage[i].id_Product+'</th>' + '<th>'+listProductLocalStorage[i].productName+'</th>'+ '<th>'+listProductLocalStorage[i].productType+'</th>'+ '<th>'+listProductLocalStorage[i].productPrice+'</th>'+ '<th id="estoque">'+listProductLocalStorage[i].productStock+'</th>'+ '<th>'+'<input type="number" id="qtdeOrder'+i+'" class="input-number" max="'+ listProductLocalStorage[i].productStock +'"></input>' +'</th>' + '<th>'+ '<button type="button" class="btn btn-info btn-small" onclick="javascript:getSales(' + listProductLocalStorage[i].id_Product + ')">Adicionar Produto</button><br>' +'</th>'+ '</tr>';
+        html += '<tr>' + '<th>'+listProductLocalStorage[i].productCode+'</th>' + 
+        '<th>'+listProductLocalStorage[i].productName+'</th>'+ 
+        '<th>'+listProductLocalStorage[i].productType+'</th>'+ 
+        '<th>'+listProductLocalStorage[i].productPrice+'</th>'+ 
+        '<th id="estoque">'+listProductLocalStorage[i].productStock+'</th>'+ 
+        '<th>'+'<input type="number" id="qtdeOrder'+i+'" class="input-number" max="'+ listProductLocalStorage[i].productStock +'"></input>' +'</th>' + 
+        '<th>'+ '<button type="button" class="btn btn-info btn-small" onclick="javascript:getSales(' + listProductLocalStorage[i].productCode + ')">Adicionar Produto</button><br>' +'</th>'+ '</tr>';
     }
 
     $("#listSales").append(html);
 }
 
 var saleValue = 0;
-function getSales(id_Product) {
+function getSales(productCode) {
     listProductLocalStorage = JSON.parse(localStorage.getItem('product'));
 
-    var pedido = document.getElementById("qtdeOrder"+(id_Product-1)).value;
-    var preco = parseFloat(listProductLocalStorage[id_Product-1].productPrice);
+    var pedido = document.getElementById("qtdeOrder"+(productCode-1)).value;
+    var preco = parseFloat(listProductLocalStorage[productCode-1].productPrice);
     var somaPedido = preco*pedido;
 
     saleValue += somaPedido;
 
     $("#saleValue").val(saleValue);
 
-    var prodName = listProductLocalStorage[id_Product-1].productName;
+    var prodName = listProductLocalStorage[productCode-1].productName;
     $("#ordList").append(prodName);
 
 }

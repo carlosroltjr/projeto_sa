be057abce415bdeed79996f0fb4fb88ed1a0b355
Carlos De Rolt Jr <carlosrderolt@gmail.com>
2019-06-24 19:32:24 -0300

    arrumei a navegação do dashboard

--- _SAbootstrap/js/check-logged-adm.js
+++ _SAbootstrap/js/check-logged-adm.js
@@ -9,6 +9,7 @@ if (isLogged){
     document.getElementById('sales').onclick = function(){ return false }
   } else{
     document.getElementById('foodtrucks').onclick = function(){ return false }
+    document.getElementById('charts').onclick = function(){ return false }
   }
 } else{
   alert("Você não está logado!");
